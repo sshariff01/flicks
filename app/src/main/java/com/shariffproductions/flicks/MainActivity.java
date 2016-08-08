522f8a94ab577ac1a5656b9b03bfb3cd5162e174
Shoabe Shariff <shoabe.shariff@gmail.com>
2016-08-07 19:02:37 -0700

    Ellipsize after 8 lines of synopsis

--- app/src/main/java/com/shariffproductions/flicks/MainActivity.java
+++ app/src/main/java/com/shariffproductions/flicks/MainActivity.java
@@ -35,18 +35,6 @@ public class MainActivity extends Activity {
         populateMovieListings();
     }
 
-    @Override
-    public void onConfigurationChanged(Configuration newConfig) {
-        super.onConfigurationChanged(newConfig);
-
-        // Checks the orientation of the screen
-        if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
-            Toast.makeText(this, "landscape", Toast.LENGTH_SHORT).show();
-        } else if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT){
-            Toast.makeText(this, "portrait", Toast.LENGTH_SHORT).show();
-        }
-    }
-
     private void populateMovieListings() {
         HttpClient httpClient = new HttpClient();
         httpClient.getNowPlayingMovies(new JsonHttpResponseHandler() {
